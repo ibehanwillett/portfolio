e8c1c5a79f32a0bd7d523499b929552bc77dd0a9
ibehanwillett <imogenbdubs@gmail.com>
2024-02-01 15:03:25 +1000

    basic homepage return down.

--- website-portfolio/src/App.jsx
+++ website-portfolio/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import mainpic from './assets/mainpic.jpg'
 import './App.css'
 
 function App() {
@@ -8,26 +7,26 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div >
+        <div id="name">
+            <h1>Imogen</h1>
+            <h1>Behan-</h1>
+            <h1>Willett</h1>
+        </div>
+            <img id="headerpic" src={mainpic} alt="A picture of Imogen Behan-Willett"/> 
+            <p>Web developer.</p>
+    </div>
+    <div id="buttons">
+      <button>
+        ABOUT
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button>
+        PROJECTS
+        </button>
+        <button>
+          CONTACT
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
