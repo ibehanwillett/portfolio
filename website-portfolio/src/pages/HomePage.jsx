90786e713fc4f0163108754cb5fa2ae5bf1ed552
ibehanwillett <imogenbdubs@gmail.com>
2024-02-02 12:31:11 +1000

    added additional styling to homepage.

--- website-portfolio/src/pages/HomePage.jsx
+++ website-portfolio/src/pages/HomePage.jsx
@@ -1,21 +1,26 @@
 import mainpic from '../assets/mainpic.jpg'
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
+import './styles/home-style.css'
 
 export function HomePage(props){
     return (
         <>
-        <div >
+        <div id="header">  
+         <img id="headerpic" src={mainpic} alt="A picture of Imogen Behan-Willett"/>  
             <div id="name">
                 <h1>Imogen</h1>
                 <h1>Behan-</h1>
                 <h1>Willett</h1>
-            </div>
-                <img id="headerpic" src={mainpic} alt="A picture of Imogen Behan-Willett"/> 
                 <p>Web developer.</p>
+            </div>
+           
+                
         </div>
         <div id="buttons">
             <Link to="/about">ABOUT</Link>
+            <br/>
             <Link to="/about">PROJECTS</Link>
+            <br/>
             <Link to="/about">CONTACT</Link>
             </div>
         </>
