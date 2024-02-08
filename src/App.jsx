07ee3c909a34e121a5d7a3b9bd6678cf3456cd5a
ibehanwillett <imogenbdubs@gmail.com>
2024-02-08 15:58:01 +1000

    moved all files one directory up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { HomePage } from './pages/HomePage'
-import { AboutPage } from './pages/AboutPage'
-import { ContactPage } from './pages/ContactPage'
-import { ProjectsPage } from './pages/ProjectsPage'
+import { HomePage } from './pages/HomePage.jsx'
+import { AboutPage } from './pages/AboutPage.jsx'
+import { ContactPage } from './pages/ContactPage.jsx'
+import { ProjectsPage } from './pages/ProjectsPage.jsx'
 import Footer from './components/Footer.jsx'
 
 
